00a3eab7c47d99073c195b824d8ced704132a8a9
Adrien Piette <you@example.com>
2025-12-09 12:05:51 +0100

    test_pdf_extraction fonctionne

--- test/test_pdf_extraction.py
+++ test/test_pdf_extraction.py
@@ -1,3 +1,9 @@
 from ingestion.fonctions_extraction import extract_text_pdf
 
-pdf_path = 
+pdf_path = "data/pdf/Gestion des Lits.pdf"
+
+pages = extract_text_pdf(pdf_path) 
+
+print(f"Number of pages extracted: {len(pages)}")
+print("Content of the first page:")
+print(pages[0])
